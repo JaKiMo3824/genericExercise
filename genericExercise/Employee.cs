b31d2a10028097199731bfc55f21c5b5f855881e
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-23 12:07:31 -0700

    genericExercise
    generic exercises 1-5

---
+++ genericExercise/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericExercise
+{
+    class Employee<T> : Person//, IQuittable
+    {
+
+        public List<T> things = new List<T>();
+
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            string fullName = FirstName + " " + LastName;
+            Console.WriteLine($"Name: {fullName}");
+            Console.ReadLine();
+        }
+
+        //public static bool operator ==(Employee employee, Employee employee2)
+        //{
+        //    if (employee.Id == employee2.Id)
+        //    {
+
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+
+        //}
+        //public static bool operator !=(Employee employee, Employee employee2)
+        //{
+        //    if (employee.Id != employee2.Id)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+
+
+        //public void Quit()
+        //{
+        //    if (Employed == false)
+        //    {
+        //        string fullName = FirstName + " " + LastName;
+        //        Console.WriteLine(fullName + " is no longer employed.");
+        //        Console.ReadLine();
+        //    }
+
+
+        //}
+    }
+}
--- genericExercise/Program.cs
+++ genericExercise/Program.cs
@@ -10,6 +10,56 @@ namespace genericExercise
     {
         static void Main(string[] args)
         {
+            Employee<string> Strings = new Employee<string>(); 
+            Strings.things = new List<string>() {"Dog", "Cat", "Bird", "Mouse" };
+
+            Employee<int> Integers = new Employee<int>();
+            Integers.things = new List<int>() { 1, 2, 3, 4 };
+
+            
+
+
+            foreach (string stri in Strings.things)
+            {
+                Console.WriteLine(stri);
+
+                
+            }
+            foreach (int inte in Integers.things)
+            {
+                    Console.WriteLine(inte);
+                    
+            }
+            Console.ReadLine();
+            //Console.WriteLine(Strings.things);
+            //Console.WriteLine(Integers.things);
+            //Console.ReadLine();
+
+
+            //Employee employee = new Employee();
+            //employee.FirstName = "Sample";
+            //employee.LastName = "Student";
+            //employee.Employed = true;
+            //employee.Id = 1234;
+
+            //Employee employee2 = new Employee();
+            //employee2.FirstName = "Ample";
+            //employee2.LastName = "Tudent";
+            //employee2.Employed = true;
+            //employee2.Id = 1234;
+
+            //if (employee.Id == employee2.Id)
+            //{
+            //    Console.WriteLine("The two employee ID's are identical and therefore are invalid.");
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The two employee ID's are not identical.");
+            //    Console.ReadLine();
+            //}
+
+            //employee.Quit();
         }
     }
 }
